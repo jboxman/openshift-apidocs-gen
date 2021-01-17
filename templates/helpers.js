c83477bf0a5c8b04aa52a78530b3e0e25e54e048
Jason Boxman <jasonb@edseek.com>
2021-01-17 12:43:14 -0500

    Omit leading `.` in property name
    Closes #15

--- templates/helpers.js
+++ templates/helpers.js
@@ -69,6 +69,9 @@ const parametersFor = (params = [], where = '') => {
 };
 
 const truncatePath = (path, parent) => {
+  if(parent == '.')
+    return path.replace(`.`, '');
+
   return path.replace(`${parent}.`, '');
 };
 
